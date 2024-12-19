27409d490a3f65b937de4bbdd94e9dd40e99f4d8
ItachiPrabin <itachiprabin@gmail.com>
2024-12-20 00:36:01 +0545

    added navbar in homepage

--- lib/view/home_page_view.dart
+++ lib/view/home_page_view.dart
@@ -1,22 +1,62 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:clean_ease/common/navigator.dart';
+import 'package:clean_ease/view/calendar_view.dart';
+import 'package:clean_ease/view/order_view.dart';
+import 'package:clean_ease/view/settings_view.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreenView extends StatelessWidget {
+class HomeScreenView extends StatefulWidget {
   const HomeScreenView({super.key});
 
   @override
+  State<HomeScreenView> createState() => _HomeScreenViewState();
+}
+
+class _HomeScreenViewState extends State<HomeScreenView> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const CalendarView()),
+        );
+        break;
+      case 2:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const OrderView()),
+        );
+        break;
+      case 3:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const SettingsView()),
+        );
+        break;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.cyan,
-        title: const Row(
+        title: Row(
           children: [
-            CircleAvatar(
+            const CircleAvatar(
               radius: 20,
               backgroundImage: AssetImage('assets/images/image1.jpg'),
             ),
-            SizedBox(width: 10),
-            Column(
+            SizedBox(width: screenWidth * 0.02),
+            const Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
@@ -42,181 +82,146 @@ class HomeScreenView extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // First Section: Our Services
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 4,
-                      offset: Offset(2, 2),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Our Services',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    GridView.count(
-                      shrinkWrap: true,
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 16.0,
-                      mainAxisSpacing: 16.0,
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: [
-                        _buildServiceCard(
-                          context,
-                          'Automated Laundry',
-                          'assets/images/washingmachine.png',
-                          Colors.orange,
-                          const Text(
-                            'Automated Laundry Page',
-                          ),
-                        ),
-                        _buildServiceCard(
-                            context,
-                            'Dry Cleaning',
-                            'assets/images/folded clothes.png',
-                            Colors.orange,
-                            const Text(
-                              'Dry Cleaning Page',
-                            )),
-                        _buildServiceCard(
-                            context,
-                            'Only Press',
-                            'assets/images/iron.jpg',
-                            Colors.orange,
-                            const Text(
-                              'Only Press Page',
-                            )),
-                        _buildServiceCard(
-                            context,
-                            'Shoe Cleaning',
-                            'assets/images/shoe.jpg',
-                            Colors.orange,
-                            const Text(
-                              'Shoe Cleaning Page',
-                            )),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              // Second Section: Price List
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 4,
-                      offset: Offset(2, 2),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Price List',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.black54,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              // Third Section: Our Offers
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 4,
-                      offset: Offset(2, 2),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Our Offers',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    _buildPromotionalBanner(),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: _getSelectedScreen(_selectedIndex),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.red,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'Calendar',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag),
-            label: 'Order',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+    );
+  }
+
+  Widget _getSelectedScreen(int index) {
+    switch (index) {
+      case 0:
+        return _buildHomeScreen();
+      default:
+        return _buildHomeScreen();
+    }
+  }
+
+  Widget _buildHomeScreen() {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(screenWidth * 0.04), // Responsive padding
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // First Section: Our Services
+            Container(
+              padding: EdgeInsets.all(screenWidth * 0.04),
+              decoration: _boxDecoration(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Our Services',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  LayoutBuilder(
+                    builder: (context, constraints) {
+                      int crossAxisCount = constraints.maxWidth > 600 ? 3 : 2;
+
+                      return GridView.count(
+                        shrinkWrap: true,
+                        crossAxisCount: crossAxisCount,
+                        crossAxisSpacing: 16.0,
+                        mainAxisSpacing: 16.0,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: [
+                          _buildServiceCard(
+                              context,
+                              'Automated Laundry',
+                              'assets/images/washingmachine.png',
+                              Colors.orange,
+                              const Text('Automated Laundry Page')),
+                          _buildServiceCard(
+                              context,
+                              'Dry Cleaning',
+                              'assets/images/folded clothes.png',
+                              Colors.orange,
+                              const Text('Dry Cleaning Page')),
+                          _buildServiceCard(
+                              context,
+                              'Only Press',
+                              'assets/images/iron.jpg',
+                              Colors.orange,
+                              const Text('Only Press Page')),
+                          _buildServiceCard(
+                              context,
+                              'Shoe Cleaning',
+                              'assets/images/shoe.jpg',
+                              Colors.orange,
+                              const Text('Shoe Cleaning Page')),
+                        ],
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Second Section: Price List
+            Container(
+              padding: EdgeInsets.symmetric(
+                vertical: screenWidth * 0.03,
+                horizontal: screenWidth * 0.04,
+              ),
+              decoration: _boxDecoration(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Price List',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Third Section: Our Offers
+            Container(
+              padding: EdgeInsets.all(screenWidth * 0.04),
+              decoration: _boxDecoration(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Our Offers',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  _buildPromotionalBanner(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -243,12 +248,6 @@ class HomeScreenView extends StatelessWidget {
           'description': 'Limited time offer. Don\'t miss out!',
           'color': const Color.fromARGB(255, 85, 163, 226),
           'image': 'assets/images/best.png',
-        },
-        {
-          'title': 'Exclusive 10% Off on Laundry!',
-          'description': 'Book now and save big!',
-          'color': const Color.fromARGB(255, 55, 157, 240),
-          'image': 'assets/images/folded clothes.png',
         },
       ].map((offer) {
         final String title = offer['title'] as String;
@@ -341,8 +340,6 @@ class HomeScreenView extends StatelessWidget {
             Expanded(
               child: Image.asset(
                 assetPath,
-                height: 120,
-                width: 140,
                 fit: BoxFit.contain,
               ),
             ),
@@ -359,6 +356,20 @@ class HomeScreenView extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  BoxDecoration _boxDecoration() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(12),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black12,
+          blurRadius: 4,
+          offset: Offset(2, 2),
+        ),
+      ],
     );
   }
 }
