fd3e99888a22aba21921a95b52fa966a56d3ef2d
ItachiPrabin <itachiprabin@gmail.com>
2024-12-20 16:07:18 +0545

    updated app.dart for theme and fonts

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:clean_ease/core/app_theme/app_theme.dart';
 import 'package:clean_ease/view/splash_screen_view.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
       routes: {
         '/': (context) => const SplashScreenView(),
       },
