4028b534c0483204003a3034bf48c15b63ec2df6
ItachiPrabin <itachiprabin@gmail.com>
2025-02-07 18:41:23 +0545

    changed login use case for test

--- lib/features/auth/domain/use_case/login_usecase.dart
+++ lib/features/auth/domain/use_case/login_usecase.dart
@@ -1,9 +1,44 @@
+// import 'package:dartz/dartz.dart';
+// import 'package:equatable/equatable.dart';
+
+// import '../../../../app/usecase/usecase.dart';
+// import '../../../../core/error/failure.dart';
+// import '../repository/auth_repository.dart';
+
+// class LoginParams extends Equatable {
+//   final String email;
+//   final String password;
+
+//   const LoginParams({
+//     required this.email,
+//     required this.password,
+//   });
+
+//   const LoginParams.initial()
+//       : email = '',
+//         password = '';
+
+//   @override
+//   List<Object> get props => [email, password];
+// }
+
+// class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
+//   final IAuthRepository repository;
+
+//   LoginUseCase(this.repository);
+
+//   @override
+//   Future<Either<Failure, String>> call(LoginParams params) {
+//     return repository.loginUser(params.email, params.password);
+//   }
+// }
+
+import 'package:clean_ease/app/shared_prefs/token_shared_prefs.dart';
+import 'package:clean_ease/app/usecase/usecase.dart';
+import 'package:clean_ease/core/error/failure.dart';
+import 'package:clean_ease/features/auth/domain/repository/auth_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-
-import '../../../../app/usecase/usecase.dart';
-import '../../../../core/error/failure.dart';
-import '../repository/auth_repository.dart';
 
 class LoginParams extends Equatable {
   final String email;
@@ -14,6 +49,7 @@ class LoginParams extends Equatable {
     required this.password,
   });
 
+  // Initial Constructor
   const LoginParams.initial()
       : email = '',
         password = '';
@@ -24,11 +60,22 @@ class LoginParams extends Equatable {
 
 class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
   final IAuthRepository repository;
+  final TokenSharedPrefs tokenSharedPrefs;
 
-  LoginUseCase(this.repository);
+  LoginUseCase(this.repository, this.tokenSharedPrefs);
 
   @override
-  Future<Either<Failure, String>> call(LoginParams params) {
-    return repository.loginUser(params.email, params.password);
+  Future<Either<Failure, String>> call(LoginParams params) async {
+    final result = await repository.loginUser(params.email, params.password);
+
+    return result.fold(
+      (failure) => Left(failure), // Return failure if login fails
+      (token) async {
+        await tokenSharedPrefs.saveToken(token);
+        final savedToken = await tokenSharedPrefs.getToken();
+        print(savedToken);
+        return Right(token);
+      },
+    );
   }
 }
