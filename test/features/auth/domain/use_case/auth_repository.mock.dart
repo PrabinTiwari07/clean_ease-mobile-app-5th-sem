ac2a9889cb1bf419eb5978832914f5a0aa7f9d7a
ItachiPrabin <itachiprabin@gmail.com>
2025-02-07 18:15:14 +0545

    auth mock repository

---
+++ test/features/auth/domain/use_case/auth_repository.mock.dart
@@ -0,0 +1,4 @@
+import 'package:clean_ease/features/auth/domain/repository/auth_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class AuthRepoMock extends Mock implements IAuthRepository {}
