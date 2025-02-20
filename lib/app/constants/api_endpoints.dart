671e6f6565e2c263325d321ea493502fd0ff3e82
ItachiPrabin <itachiprabin@gmail.com>
2025-02-20 13:26:09 +0545

    constants

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -14,4 +14,23 @@ class ApiEndpoints {
   static const String verifyEmail = "users/verify-otp";
   // static const String imageUrl = "http://10.0.2.2:1500/uploads/";
   static const String uploadImage = "auth/uploadImage";
+
+  // ====================== Profile Endpoints ======================
+  static const String getUserProfile = "/user/profile";
+  static const String updateUserProfile = "/user/update";
+  static const String changePassword = "/user/change-password";
+  static const String logout = "/user/logout";
+
+  // ====================== Booking Routes ======================
+  // static const String bookService = "bookings/book";
+  // static const String getAllBookings = "bookings/all";
+  // static const String getUserBookings = "bookings/getbooking";
+  // static const String confirmBooking = "bookings/confirm";
+
+  // ====================== Service Routes ======================
+  static const String getAllServices = "services/";
+  static const String getServiceById = "services/";
+  static const String createService = "services/";
+  static const String updateServiceAvailability = "services/";
+  static const String deleteService = "services/";
 }
