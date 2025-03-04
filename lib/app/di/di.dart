15039cee77e8b7216abb1df3aa3271bb83ccaa63
ItachiPrabin <itachiprabin@gmail.com>
2025-03-04 09:16:55 +0545

    bookin in di

--- lib/app/di/di.dart
+++ lib/app/di/di.dart
@@ -1,292 +1,3 @@
-// //===============================================//-------------------------------------------------------------------------------------------
-// import 'package:clean_ease/app/shared_prefs/token_shared_prefs.dart';
-// import 'package:clean_ease/core/network/api_service.dart';
-// import 'package:clean_ease/core/network/hive_service.dart';
-// import 'package:clean_ease/features/auth/data/data_source/local_data_source/auth_local_data_source.dart';
-// import 'package:clean_ease/features/auth/data/data_source/remote_data_source/auth_remote_data_source.dart';
-// import 'package:clean_ease/features/auth/data/repository/auth_local_repository.dart';
-// import 'package:clean_ease/features/auth/data/repository/auth_remote_repository/auth_remote_repository.dart';
-// import 'package:clean_ease/features/auth/domain/use_case/login_usecase.dart';
-// import 'package:clean_ease/features/auth/domain/use_case/register_usecase.dart';
-// import 'package:clean_ease/features/auth/domain/use_case/upload_image_usecase.dart';
-// import 'package:clean_ease/features/auth/domain/use_case/verify_usecase.dart';
-// import 'package:clean_ease/features/auth/presentation/view_model/login/login_bloc.dart';
-// import 'package:clean_ease/features/auth/presentation/view_model/signup/register_bloc.dart';
-// import 'package:clean_ease/features/home/presentation/view_model/home_cubit.dart';
-// import 'package:clean_ease/features/profile/data/data_source/local_data_source/profile_local_data_source.dart';
-// import 'package:clean_ease/features/profile/data/data_source/remote_data_source/profile_remote_data_source.dart';
-// import 'package:clean_ease/features/profile/data/repository/profile_repository.dart';
-// import 'package:clean_ease/features/profile/domain/repository/profile_repository.dart';
-// import 'package:clean_ease/features/profile/domain/use_case/change_password_use_case.dart';
-// import 'package:clean_ease/features/profile/domain/use_case/get_profile_use_case.dart';
-// import 'package:clean_ease/features/profile/domain/use_case/logout_use_case.dart';
-// import 'package:clean_ease/features/profile/domain/use_case/update_profile_use_case.dart';
-// import 'package:clean_ease/features/profile/presentation/view_model/profile_block.dart';
-// import 'package:clean_ease/features/service/data/data_source/local_data_source/service_local_data_source.dart';
-// import 'package:clean_ease/features/service/data/data_source/remote_data_source/service_remote_data_source.dart';
-// import 'package:clean_ease/features/service/data/repository/service_local_repository.dart';
-// import 'package:clean_ease/features/service/data/repository/service_remote_repository.dart';
-// import 'package:clean_ease/features/service/domain/use_case/get_service_by_id_usecase.dart';
-// import 'package:clean_ease/features/service/domain/use_case/service_use_case.dart';
-// import 'package:clean_ease/features/service/presentation/view_model/service_block.dart';
-// import 'package:dio/dio.dart';
-// import 'package:get_it/get_it.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// final getIt = GetIt.instance;
-
-// Future<void> initDependencies() async {
-//   _initHiveService();
-//   _initApiService();
-//   await _initSharedPreferences();
-//   await _initLoginDependencies();
-//   await _initSignupDependencies();
-//   _initHomeDependencies();
-//   _initServiceDependencies();
-//   _initProfileDependencies(); // ✅ Added Profile Dependencies
-// }
-
-// Future<void> _initSharedPreferences() async {
-//   final sharedPreferences = await SharedPreferences.getInstance();
-//   getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
-// }
-
-// void _initApiService() {
-//   getIt.registerLazySingleton<Dio>(
-//     () => ApiService(Dio()).dio,
-//   );
-// }
-
-// void _initHiveService() {
-//   getIt.registerLazySingleton<HiveService>(() => HiveService());
-// }
-
-// void _initHomeDependencies() {
-//   getIt.registerFactory<HomeCubit>(
-//     () => HomeCubit(),
-//   );
-// }
-
-// void _initServiceDependencies() {
-//   // Service Data Sources
-//   getIt.registerLazySingleton<ServiceLocalDataSource>(
-//     () => ServiceLocalDataSource(hiveService: getIt<HiveService>()),
-//   );
-
-//   getIt.registerLazySingleton<ServiceRemoteDataSource>(
-//     () => ServiceRemoteDataSource(dio: getIt<Dio>()),
-//   );
-
-//   // Service Repositories
-//   getIt.registerLazySingleton<ServiceLocalRepository>(
-//     () => ServiceLocalRepository(
-//         serviceLocalDataSource: getIt<ServiceLocalDataSource>()),
-//   );
-
-//   getIt.registerLazySingleton<ServiceRemoteRepository>(
-//     () => ServiceRemoteRepository(
-//         serviceRemoteDataSource: getIt<ServiceRemoteDataSource>()),
-//   );
-
-//   // Service Use Cases
-//   getIt.registerLazySingleton<GetServicesUseCase>(
-//     () =>
-//         GetServicesUseCase(serviceRepository: getIt<ServiceRemoteRepository>()),
-//   );
-
-//   getIt.registerLazySingleton<GetServiceByIdUseCase>(
-//     () => GetServiceByIdUseCase(
-//         serviceRepository: getIt<ServiceRemoteRepository>()),
-//   );
-
-//   // Service Bloc
-//   getIt.registerFactory<ServiceBloc>(
-//     () => ServiceBloc(
-//       getServicesUseCase: getIt<GetServicesUseCase>(),
-//       getServiceByIdUseCase: getIt<GetServiceByIdUseCase>(),
-//     ),
-//   );
-// }
-
-// // //  ✅ **NEW: Register Profile Dependencies**
-// // // void _initProfileDependencies() {
-// // //   // ✅ Ensure Hive Box is Opened Before Injecting It
-// // //   final profileBox = Hive.box<ProfileHiveModel>('profile_box');
-
-// // //   getIt.registerLazySingleton<ProfileLocalDataSource>(
-// // //     () => ProfileLocalDataSource(profileBox: profileBox),
-// // //   );
-
-// // //   // ✅ Register Profile Remote Data Source
-// // //   getIt.registerLazySingleton<ProfileRemoteDataSource>(
-// // //     () => ProfileRemoteDataSource(dio: getIt<Dio>()),
-// // //   );
-// // void _initProfileDependencies() {
-// //   // ✅ Ensure Hive Box is Open Before Injecting It
-// //   if (!Hive.isBoxOpen('profile_box')) {
-// //     throw Exception(
-// //         "HiveError: profile_box is not open. Make sure to call Hive.openBox('profile_box') in main.dart.");
-// //   }
-
-// //   final profileBox = Hive.box<ProfileHiveModel>('profile_box');
-
-// //   getIt.registerLazySingleton<ProfileLocalDataSource>(
-// //     () => ProfileLocalDataSource(profileBox: profileBox),
-// //   );
-
-// //   // ✅ Register Profile Repository
-// //   getIt.registerLazySingleton<ProfileRepository>(
-// //     () => ProfileRepositoryImpl(
-// //       remoteDataSource: getIt<ProfileRemoteDataSource>(),
-// //       localDataSource: getIt<ProfileLocalDataSource>(),
-// //     ),
-// //   );
-
-// //   // ✅ Register Use Cases
-// //   getIt.registerLazySingleton<GetProfileUseCase>(
-// //     () => GetProfileUseCase(getIt<ProfileRepository>()),
-// //   );
-
-// //   getIt.registerLazySingleton<UpdateProfileUseCase>(
-// //     () => UpdateProfileUseCase(getIt<ProfileRepository>()),
-// //   );
-
-// //   getIt.registerLazySingleton<ChangePasswordUseCase>(
-// //     () => ChangePasswordUseCase(getIt<ProfileRepository>()),
-// //   );
-
-// //   getIt.registerLazySingleton<LogoutUseCase>(
-// //     () => LogoutUseCase(getIt<ProfileRepository>()),
-// //   );
-
-// //   // ✅ Register ProfileBloc
-// //   getIt.registerFactory<ProfileBloc>(
-// //     () => ProfileBloc(
-// //       getProfileUseCase: getIt<GetProfileUseCase>(),
-// //       updateProfileUseCase: getIt<UpdateProfileUseCase>(),
-// //       changePasswordUseCase: getIt<ChangePasswordUseCase>(),
-// //       logoutUseCase: getIt<LogoutUseCase>(),
-// //     ),
-// //   );
-// // }
-
-// void _initProfileDependencies() {
-//   // ✅ Ensure Hive Box is Open Before Injecting It
-//   // if (!Hive.isBoxOpen('profile_box')) {
-//   // throw Exception(
-//   // "HiveError: profile_box is not open. Make sure to call Hive.openBox('profile_box') in main.dart before running initDependencies().");
-//   // }
-
-//   // final profileBox = Hive.box<ProfileHiveModel>('profile_box');
-
-//   // getIt.registerLazySingleton<ProfileLocalDataSource>(
-//   // () => ProfileLocalDataSource(profileBox: profileBox),
-//   // );
-
-//   // ✅ Register Profile Remote Data Source
-//   getIt.registerLazySingleton<ProfileRemoteDataSource>(
-//     () => ProfileRemoteDataSource(dio: getIt<Dio>()),
-//   );
-
-//   // ✅ Register Profile Repository
-//   getIt.registerLazySingleton<ProfileRepository>(
-//     () => ProfileRepositoryImpl(
-//       remoteDataSource: getIt<ProfileRemoteDataSource>(),
-//       localDataSource: getIt<ProfileLocalDataSource>(),
-//     ),
-//   );
-
-//   // ✅ Register Use Cases
-//   getIt.registerLazySingleton<GetProfileUseCase>(
-//     () => GetProfileUseCase(getIt<ProfileRepository>()),
-//   );
-
-//   getIt.registerLazySingleton<UpdateProfileUseCase>(
-//     () => UpdateProfileUseCase(getIt<ProfileRepository>()),
-//   );
-
-//   getIt.registerLazySingleton<ChangePasswordUseCase>(
-//     () => ChangePasswordUseCase(getIt<ProfileRepository>()),
-//   );
-
-//   getIt.registerLazySingleton<LogoutUseCase>(
-//     () => LogoutUseCase(getIt<ProfileRepository>()),
-//   );
-
-//   // ✅ Register ProfileBloc
-//   getIt.registerFactory<ProfileBloc>(
-//     () => ProfileBloc(
-//       getProfileUseCase: getIt<GetProfileUseCase>(),
-//       updateProfileUseCase: getIt<UpdateProfileUseCase>(),
-//       changePasswordUseCase: getIt<ChangePasswordUseCase>(),
-//       logoutUseCase: getIt<LogoutUseCase>(),
-//     ),
-//   );
-// }
-
-// // **FIXED: Added missing VerifyEmailUsecase registration**
-// Future<void> _initSignupDependencies() async {
-//   getIt.registerLazySingleton<AuthLocalDataSource>(
-//     () => AuthLocalDataSource(getIt<HiveService>()),
-//   );
-
-//   getIt.registerLazySingleton<AuthRemoteDataSource>(
-//     () => AuthRemoteDataSource(getIt<Dio>()),
-//   );
-
-//   getIt.registerLazySingleton<AuthLocalRepository>(
-//     () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
-//   );
-
-//   getIt.registerLazySingleton<AuthRemoteRepository>(
-//     () => AuthRemoteRepository(getIt<AuthRemoteDataSource>()),
-//   );
-
-//   getIt.registerLazySingleton<RegisterUseCase>(
-//     () => RegisterUseCase(getIt<AuthRemoteRepository>()),
-//   );
-
-//   getIt.registerLazySingleton<UploadImageUsecase>(
-//     () => UploadImageUsecase(getIt<AuthRemoteRepository>()),
-//   );
-
-//   // **FIXED: Register VerifyEmailUsecase before using it**
-//   getIt.registerLazySingleton<VerifyEmailUsecase>(
-//     () => VerifyEmailUsecase(getIt<AuthRemoteRepository>()),
-//   );
-
-//   getIt.registerFactory<RegisterBloc>(
-//     () => RegisterBloc(
-//       registerUseCase: getIt<RegisterUseCase>(),
-//       verifyEmailUsecase: getIt<VerifyEmailUsecase>(),
-//       // uploadImageUsecase:
-//       // getIt<UploadImageUsecase>(), // Fixed: Added UploadImageUsecase
-//     ),
-//   );
-// }
-
-// // **FIXED: Ensure TokenSharedPrefs is injected in LoginUseCase**
-// Future<void> _initLoginDependencies() async {
-//   getIt.registerLazySingleton<TokenSharedPrefs>(
-//     () => TokenSharedPrefs(getIt<SharedPreferences>()),
-//   );
-
-//   getIt.registerLazySingleton<LoginUseCase>(
-//     () => LoginUseCase(
-//       getIt<AuthRemoteRepository>(),
-//       getIt<TokenSharedPrefs>(),
-//     ),
-//   );
-
-//   getIt.registerFactory<LoginBloc>(
-//     () => LoginBloc(
-//       homeCubit: getIt<HomeCubit>(),
-//       loginUseCase: getIt<LoginUseCase>(),
-//     ),
-//   );
-// }
-// //--------------------------------------------------------------------------------------------------------------------------------------------
-
 import 'package:clean_ease/app/shared_prefs/token_shared_prefs.dart';
 import 'package:clean_ease/core/network/api_service.dart';
 import 'package:clean_ease/core/network/hive_service.dart';
@@ -300,6 +11,10 @@ import 'package:clean_ease/features/auth/domain/use_case/upload_image_usecase.da
 import 'package:clean_ease/features/auth/domain/use_case/verify_usecase.dart';
 import 'package:clean_ease/features/auth/presentation/view_model/login/login_bloc.dart';
 import 'package:clean_ease/features/auth/presentation/view_model/signup/register_bloc.dart';
+import 'package:clean_ease/features/booking/data/repository/booking_repository.dart';
+import 'package:clean_ease/features/booking/domain/repository/booking_repository.dart';
+import 'package:clean_ease/features/booking/domain/use_case/booking_use_case.dart';
+import 'package:clean_ease/features/booking/presentation/view_model/booking_block.dart';
 import 'package:clean_ease/features/home/presentation/view_model/home_cubit.dart';
 import 'package:clean_ease/features/price/data/data_source/price_remote_data_source.dart';
 import 'package:clean_ease/features/price/data/repository/price_repository.dart';
@@ -332,6 +47,7 @@ Future<void> initDependencies() async {
   _initServiceDependencies();
   await _initProfileDependencies();
   initPriceDependencies();
+  await _initBookingDependencies(); // ✅ Register Booking Dependencies
 }
 
 /// ✅ Ensure Hive is initialized before dependency injection
@@ -494,4 +210,37 @@ void initPriceDependencies() {
   getIt.registerFactory<PriceBloc>(
     () => PriceBloc(serviceBloc: getIt<ServiceBloc>()), // ✅ Use ServiceBloc
   );
+}
+
+Future<void> _initBookingDependencies() async {
+  // ✅ Ensure Dio is only registered once in `_initApiService()`, so do NOT register it here
+
+  // ✅ Register TokenSharedPrefs
+  // getIt.registerLazySingleton<TokenSharedPrefs>(
+  //   () => TokenSharedPrefs(getIt<SharedPreferences>()),
+  // );
+
+  // ✅ Register Booking Repository
+  getIt.registerLazySingleton<BookingRepository>(
+    () => BookingRepositoryImpl(
+      dio: getIt<Dio>(), // ✅ Use already registered Dio instance
+      tokenSharedPrefs: getIt<TokenSharedPrefs>(),
+    ),
+  );
+
+  // ✅ Register Booking Use Case
+  getIt.registerLazySingleton<BookingUseCase>(
+    () => BookingUseCase(
+      bookingRepository: getIt<BookingRepository>(),
+    ),
+  );
+
+  // ✅ Check if BookingBloc is already registered before registering
+  if (!getIt.isRegistered<BookingBloc>()) {
+    getIt.registerFactory<BookingBloc>(
+      () => BookingBloc(
+        bookingUseCase: getIt<BookingUseCase>(),
+      ),
+    );
+  }
 }
