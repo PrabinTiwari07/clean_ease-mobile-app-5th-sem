b3d8da9d6cca038518d8070496355a2290d401c0
ItachiPrabin <itachiprabin@gmail.com>
2025-03-04 09:17:44 +0545

    booking

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,8 @@
-// import 'package:clean_ease/app/di/di.dart';
-// import 'package:clean_ease/features/auth/data/model/auth_hive_model.dart';
-// import 'package:clean_ease/features/auth/presentation/view_model/login/login_bloc.dart';
-// import 'package:clean_ease/features/auth/presentation/view_model/signup/register_bloc.dart';
-// import 'package:clean_ease/features/home/presentation/view_model/home_cubit.dart';
-// import 'package:clean_ease/features/splash/presentation/view/splash_screen.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:hive_flutter/adapters.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   await initDependencies();
-//   await Hive.initFlutter();
-//   Hive.registerAdapter(UserHiveModelAdapter());
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiBlocProvider(
-//       providers: [
-//         BlocProvider<LoginBloc>(
-//           create: (_) => getIt<LoginBloc>(),
-//         ),
-//         BlocProvider<RegisterBloc>(
-//           create: (_) => getIt<RegisterBloc>(),
-//         ),
-//         BlocProvider<HomeCubit>(
-//           create: (_) => getIt<HomeCubit>(),
-//         ),
-//       ],
-//       child: MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         title: 'clean_ease',
-//         theme: ThemeData(
-//           primarySwatch: Colors.teal,
-//         ),
-//         home: const SplashScreen(),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:clean_ease/app/di/di.dart';
 import 'package:clean_ease/features/auth/data/model/auth_hive_model.dart';
 import 'package:clean_ease/features/auth/presentation/view_model/login/login_bloc.dart';
 import 'package:clean_ease/features/auth/presentation/view_model/signup/register_bloc.dart';
+import 'package:clean_ease/features/booking/presentation/view_model/booking_block.dart';
 import 'package:clean_ease/features/home/presentation/view_model/home_cubit.dart';
 import 'package:clean_ease/features/price/presentation/view_model/price_bloc.dart';
 import 'package:clean_ease/features/profile/domain/use_case/get_profile_use_case.dart';
@@ -70,18 +23,6 @@ void main() async {
   await Hive.initFlutter();
 
   Hive.registerAdapter(UserHiveModelAdapter());
-  // ✅ Register Hive Adapter
-  // Hive.registerAdapter(ProfileHiveModelAdapter());
-
-  // ✅ Open Hive Box Before DI Setup
-  // await Hive.openBox<ProfileHiveModel>('profile_box');
-  // await Hive.openBox('user_box');
-
-  // ✅ Ensure all Hive Boxes are fully opened before moving forward
-  // await Future.wait([
-  //   Hive.openBox('user_box'),
-  //   Hive.openBox<ProfileHiveModel>('profile_box'),
-  // ]);
 
   runApp(const MyApp());
 }
@@ -113,6 +54,9 @@ class MyApp extends StatelessWidget {
         BlocProvider<PriceBloc>(
           create: (context) =>
               PriceBloc(serviceBloc: context.read<ServiceBloc>()),
+        ),
+        BlocProvider<BookingBloc>(
+          create: (_) => getIt<BookingBloc>(),
         ),
       ],
       child: MaterialApp(
